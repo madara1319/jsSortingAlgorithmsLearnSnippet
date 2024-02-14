48df898b1c8aa136f6020bf3cba9bc551211577d
madara1319 <mateusz.bik@op.pl>
2024-02-14 21:27:43 +0100

    wrote some comments on partition method in quicksort class

--- sortingAlgorithmsSelfCheck.js
+++ sortingAlgorithmsSelfCheck.js
@@ -1133,25 +1133,31 @@ class SortableArray
   }
   partition(left_pointer,right_pointer)
   {
+    //will take pivot always as the right most index value
     const pivot_index=right_pointer;
     let pivot=this.array[pivot_index];
+    //will make right pointer go off pivot
     right_pointer--;
     while (true)
     {
+      //start moving left pointer until value is lower than pivot
       while(this.array[left_pointer]<pivot)
       {
         left_pointer++;
       }
+      //start moving right pointer until value is higher than pivot
       while(this.array[right_pointer]>pivot)
       {
         right_pointer--;
       }
+      //if they overcome each other or are equal go to next step
       if (left_pointer>=right_pointer)
       {
         break;
       }
       else
       {
+        //if left pointer is still lower than right  we switch their places
         [this.array[left_pointer],this.array[right_pointer]]=[this.array[right_pointer],this.array[left_pointer]];
         left_pointer++;
       }
