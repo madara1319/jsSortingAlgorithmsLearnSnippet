91bd2f5e343053bde0c5846e94e252817aa81c3b
madara1319 <mateusz.bik@op.pl>
2025-02-21 19:32:12 +0100

    added ineffective solution to array ranges task gotta revise it

---
+++ overLappingRanges.js
@@ -0,0 +1,30 @@
+//task is to write function that will check array of ranges for overlapping
+//for example for [[2,4],[5,6],[8,11]] it should return true
+//and for [[1,3],[2,5]] it should return false
+
+const testArrayOne=[[2,4],[5,6],[8,11]];
+const testArrayTwo=[[1,3],[2,5]];
+
+const overlappingRanges=(array)=>{
+    let indexesArray=[];
+    for (const el of array){
+        const start=el[0]
+        const end=el[1]
+        for(let i=start; i<=end; i++ ){
+            if(!indexesArray[i])
+            {
+                indexesArray[i]=1
+            }
+            else{
+                indexesArray[i]++;
+                return false;
+            }
+        }
+            
+    }
+    return true;
+}
+console.log(testArrayOne);
+console.log(overlappingRanges(testArrayOne))
+console.log(testArrayTwo);
+console.log(overlappingRanges(testArrayTwo))
