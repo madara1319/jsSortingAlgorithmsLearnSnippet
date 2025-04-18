2168ecfd2f227eac11f38670e80e1bfa6ed9052e
madara1319 <mateusz.bik@op.pl>
2025-04-18 19:39:29 +0200

    added graph traversals

--- BFS_DFS.js
+++ BFS_DFS.js
@@ -5,8 +5,70 @@ function createNode(value, left = null, right = null) {
 
 const tree = createNode(1, createNode(2, createNode(4), createNode(5)), createNode(3, createNode(6), createNode(7)));
 //console.log(tree)
+const graph = {
+    A: ['B', 'D'],
+    B: ['A', 'C', 'E'],
+    C: ['B'],
+    D: ['A', 'E'],
+    E: ['B', 'D', 'F'],
+    F: ['E']
+}
+let test = [];
+for (let [key, value] of Object.entries(graph)) {
+    test.push({ key, value });
+}
 
-function BFS(node) {
+//console.log(Object.keys(graph)[0])
+//let shiftedElement=graph.shift()
+//console.log(shiftedElement)
+function DFS_graph_traversal(graph, start) {
+    if (!graph) {
+        return [];
+    }
+    let stack = [start];
+    let result = [];
+    let visited = new Set();
+
+    while (stack.length) {
+        let vertex = stack.pop();
+        if (!visited.has(vertex)) {
+            visited.add(vertex);
+            result.push(vertex);
+            for (const neighbor of graph[vertex]) {
+                stack.push(neighbor)
+            }
+        }
+    }
+    return result;
+}
+
+console.log('DFS graph traversal: ' + DFS_graph_traversal(graph,'A'));
+
+function BFS_graph_traversal(graph, start) {
+    if (!graph) {
+        return [];
+    }
+    let queue = [start];
+    let result = [];
+    let visited = new Set();
+
+    while (queue.length) {
+        let vertex = queue.shift();
+        if (!visited.has(vertex)) {
+
+            visited.add(vertex);
+            result.push(vertex);
+            for (const neighbor of graph[vertex]) {
+                queue.push(neighbor);
+            }
+        }
+    }
+    return result;
+}
+
+console.log('BFS graph traversal: ' + BFS_graph_traversal(graph, 'A'))
+
+function BFS_tree_traversal(node) {
     if (!node) {
         return []
     }
@@ -26,7 +88,7 @@ function BFS(node) {
     return result;
 }
 
-function DFS(node) {
+function DFS_tree_traversal(node) {
     if (!node) {
         return [];
     }
@@ -46,8 +108,8 @@ function DFS(node) {
 }
 
 
-console.log('BFS traversal -> ' + BFS(tree))
-console.log('DFS traversal -> ' + DFS(tree))
+//console.log('BFS traversal -> ' + BFS(tree))
+//console.log('DFS traversal -> ' + DFS(tree))
 
 
 var convertBST = function(root) {
