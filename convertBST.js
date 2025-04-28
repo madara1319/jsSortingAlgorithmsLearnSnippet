55f9b5da256ce324dfcccafa54822bc3c6e67263
madara1319 <mateusz.bik@op.pl>
2025-04-28 22:29:32 +0200

    solution gave good output assuming array not TreeNode gotta change way of thinking

--- convertBST.js
+++ convertBST.js
@@ -7,15 +7,51 @@
 //	│ Input: root = [0,null,1]
 //	│ Output: [1,null,1]
 
+//var convertBST = function(root) {
+//    let queue = root;
+//    let rootCopy = root;
+//    let result = [];
+//    while (queue.length) {
+//        let currentValue = queue.shift()
+//        if (currentValue !== null) {
+//            for (let i=1; i<queue.length; i+=2)
+//            {
+//                if (queue[i]!==null){
+//                    console.log(`przejscie ${i} currentValue = ${currentValue} queue[i] = ${queue[i]}`);
+//                    currentValue += queue[i];
+//                    console.log(`_____________________________`);
+//                    console.log(`po dodaniu currentValue = ${currentValue} q queue.length = ${queue.length} `);
+//                    console.log(`_____________________________`);
+//                }
+//            }
+//        }
+//        result.push(currentValue);
+//    }
+//    return result;
+//};
+//dziala na tablicach nie na treeNode
 var convertBST = function(root) {
-    let queue = root;
+    let queue = [...root];
     let result = [];
+
     while (queue.length) {
         let currentValue = queue.shift()
-        if (queue[0] !== null) {
-            currentValue += queue[0];
+        let newValue = currentValue;
+        if (currentValue !== null) {
+           for (let i=0; i<root.length; i++){
+                if (currentValue<root[i]){
+                    console.log(`i = ${i} currentValue = ${currentValue} root[i] = ${root[i]} newValue = ${newValue}`)
+                    newValue+=root[i]
+                    console.log(`_______________________________`);
+                    console.log(`po dodaniu currentValue = ${currentValue} newValue = ${newValue} root.length = ${root.length} `)
+                    console.log(`_______________________________`);
+
+                }
+           } 
         }
-        result.push(currentValue);
+
+
+        result.push(newValue);
     }
     return result;
 };
